354246761295533b02a25d9b98b6a3aada6abae7
Milad-Aijazi <syedmilad245@gmail.com>
2025-02-15 16:52:43 +0500

    all components added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import "./App.css";
+import { Navbar,Hero,Stats,Business,CardDeal,Testimonials,Clients,CTA,Footer, Billing, } from "./components";
+import styles from "./style";
+
 function App() {
   return (
-    <div className="">
-      adfsdf
+    <div className="bg-primary w-full overflow-hidden">
+      
+      <div className={`${styles.paddingX} ${styles.flexCenter}`} >
+          <div className={`${styles.boxWidth} text-white`} >
+                <Navbar/>
+          </div>
+      </div>
+
+      <div className={`${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`${styles.flexCenter} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
