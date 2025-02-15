9bfc9ceb7016ec53d8651864a47c2d9f980c8a6b
Milad-Aijazi <syedmilad245@gmail.com>
2025-02-15 20:34:48 +0500

    Get started component have been completed.

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import styles from '../style'
+import { arrowUp } from '../assets'
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <div className='w-[124px] h-[124px] bg-blue-gradient rounded-full p-[2px]'>
+        <div className={`w-[100%] h-[100%] cursor-pointer flex flex-col bg-primary rounded-full ${styles.flexCenter}`}>
+          <div className='flex flex-row items-center'>
+              <p className={`text-[18px] font-medium font-poppins leading-[23px] mr-2`}>
+                <span className='text-gradient'>Get</span>
+              </p>
+              <img src={arrowUp} alt="arrow up" />
+          </div>
+          <p className={`text-[18px] mt-1 font-medium font-poppins leading-[23px] mr-2`}>
+                <span className='text-gradient'>Started</span>
+              </p>
+        </div>
+    </div>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section
+      className={`${styles.paddingY} flex justify-between md:flex-row flex-col`}
+    >
+      <div
+        className={`flex- ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 `}
+      >
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph}`}>
+            <span className="text-white">20%</span> Discount For{" "}
+            <span className="text-white">1 Month</span> Account
+          </p>
+        </div>
+        <div className=" flex flex-row items-center w-full">
+            <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+              The Next <br className="sm:block hidden" />{" "}
+              <span className="text-gradient">Generation</span>{" "}
+            </h1>
+            <div className="ss:flex hidden ">
+                <GetStarted />
+            </div>
+        </div>
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          Payment Method.
+        </h1>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
