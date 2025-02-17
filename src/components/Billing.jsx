681dd0525d33896fe487d311b177affd14847d31
Milad-Aijazi <syedmilad245@gmail.com>
2025-02-17 16:30:25 +0500

    completed billing section.

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import styles, { layout } from "../style";
+import { bill, google, apple } from "../assets";
 
 const Billing = () => {
   return (
-    <div>Billing</div>
-  )
-}
+    <div id="product" className={`${layout.sectionReverse}`}>
+      <div className={`${layout.sectionImgReverse}`}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5] "
+        />
+        <div className="w-[50%] h-[50%] rounded-full absolute z-[3] -left-1/2 top-0 white__gradient" />
+        <div className="w-[50%] h-[50%] rounded-full absolute z-[0] -left-1/2 top-0 pink__gradient" />
+      </div>
+      <div className={`${layout.sectionInfo}`}>
+        <h4 className={`${styles.heading2}`}>Easily control your <br className="sm:block hidden" /> billing & invoicing.</h4>
+        <p className={`${styles.paragraph} max-w-[470px] md:mt-6 mt-6`}>Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
+        <div className="flex flex-wrap flex-row mt-6">
+          <img
+            src={apple}
+            alt="apple__store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="google__store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Billing
+export default Billing;
--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import styles, { layout } from "../style";
+import Button from "./Button";
+import { features } from "../constants";
+
+const FeatureCard = ({ icon, title, content, index }) => {
+  return (
+    <div
+      className={`flex flex-row rounded-[20px] feature-card p-6 ${
+        index !== features.lenght ? "mb-6" : "mb-0"
+      }`}
+    >
+      <div className="w-[64px] h-[64px] rounded-full bg-dimBlue flex justify-center items-center"> 
+        <img className="w-[50%] h-[50%] object-contain" src={icon} alt="image icon" />
+      </div>
+      <div className="flex-1 flex flex-col ml-3">
+          <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">{title}</h4>
+          <p className="font-poppins font-noraml text-[16px] leading-[24px] text-dimWhite">{content}</p>
+      </div>
+    </div>
+  );
+};
 
 const Business = () => {
   return (
-    <div>Business</div>
-  )
-}
+    <div className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          You do the business, <br className="sm:block hidden" /> we’ll handle
+          the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-4`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} index={index} {...feature} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Business
+export default Business;
