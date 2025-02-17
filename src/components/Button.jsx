4ffe20b734956ef04881e6a98f6913dc5d5b2c06
Milad-Aijazi <syedmilad245@gmail.com>
2025-02-17 12:07:25 +0500

    Button components added.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      type="button"
+      className={`text-white px-6 py-4 bg-blue-gradient font-poppins font-medium text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
