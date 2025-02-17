5be4ca14b8d52c8b2d427812a2382400de9130a9
Milad-Aijazi <syedmilad245@gmail.com>
2025-02-17 23:27:16 +0500

    CTA completed.

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from '../style'
+import Button from './Button'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div  className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} box-shadow bg-black-gradient rounded-[20px] sm:flex-row flex-col `}>
+      <div className='flex flex-col flex-1'>
+          <h4 className={`${styles.heading2}`}>Let’s try our service now!</h4>
+          <p className={`${styles.paragraph} max-w-[470px]`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+      </div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10 `}>
+        <Button />
+      </div>
+    </div>
   )
 }
 
