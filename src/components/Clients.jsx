b81d6fc809d4edc05f7f786559ad24bd7d356067
Milad-Aijazi <syedmilad245@gmail.com>
2025-02-17 19:59:45 +0500

    cleint have been done.

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { clients } from '../constants'
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <div className='flex flex-row justify-center items-center'>
+      <div className='flex w-full flex-wrap flex-row flex justify-center items-center'>
+        {clients.map((client)=>(
+          <div key={client.id}  className='flex-1 flex justify-center items-center sm:min-w-[192px] min-w-[120px]' >
+            <img src={client.logo} alt={client.id+1} className='sm:min-w-[192px] w-[100px] object-contain' />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
